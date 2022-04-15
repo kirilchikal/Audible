1ca95dc07cfea766d09ffcfec2d7dd7ad71ea5b5
Alena <kirilchik.al@gmail.com>
2022-04-15 18:00:52 +0200

    Temp data add

---
+++ src/Data/bookList.js
@@ -0,0 +1,27 @@
+const bookList = [
+    {
+      id:0,
+      title: 'One Italian summer',
+      author:'Rebeca Serle',
+      description: 'The New York Times bestselling author of In Five Years returns with a powerful novel about the transformational love between mothers and daughters set on the breathtaking Amalfi Coast. When Katy’s mother dies, she is left reeling. Carol wasn’t just Katy’s mom, but her best friend and first phone call. She had all the answers and now, when Katy needs her the most, she is gone. To make matters worse, their planned mother-daughter trip of a lifetime looms: to Positano, the magical town where Carol spent the summer right before she met Katy’s father.',
+      rating:4,
+      price:16.08,
+      category:'Romance',
+      image_url:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1626799802i/58438583.jpg",
+      audio: false,
+    },
+    {
+        id:1,
+        title: 'House of Sky and Breath',
+        author:'Sarah J. Maas',
+        description: 'Bryce Quinlan and Hunt Athalar are trying to get back to normal-they may have saved Crescent City, but with so much upheaval in their lives lately, they mostly want a chance to relax. Slow down. Figure out what the future holds. The Asteri have kept their word so far, leaving Bryce and Hunt alone. But with the rebels chipping away at the Asteri’s power, the threat the rulers pose is growing. As Bryce, Hunt, and their friends get pulled into the rebel’s plans, the choice becomes clear: stay silent while others are oppressed, or fight for what’s right. And they’ve never been very good at staying silent.',
+        rating:5,
+        price:9.99,
+        category:'Fantasy',
+        image_url:"https://m.media-amazon.com/images/I/51WApJ4MiLL.jpg",
+        audio: false,
+    }
+];
+
+
+export default bookList;
---
+++ src/Data/categoryList.js
@@ -0,0 +1,18 @@
+
+const categoryList = [
+    {
+      name: 'Fantasy',
+    },
+    {
+      name: 'Thriller',
+    },
+    {
+      name: 'Romance',
+    },
+    {
+      name: 'History',
+    },
+  ];
+export default categoryList;
+
+
