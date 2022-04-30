5e2f1c4df13c9cc09006e521c99506df46cc017d
Alena <kirilchik.al@gmail.com>
2022-04-30 16:17:37 +0200

    Login & sign up

--- src/views/SignInScreen.js
+++ src/views/SignInScreen.js
@@ -1,18 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, TouchableOpacity } from 'react-native';
+import React, {useState} from "react";
+import { StyleSheet, Text, View, SafeAreaView, Button, TouchableOpacity, TextInput } from 'react-native';
 
 export default function SignInScreen({navigation}) {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
     return (
       <SafeAreaView style={styles.container}>
-        <Text>Sign in screen</Text>
-        <Button
-            title='Go to Sign Up'
-            onPress={() => navigation.navigate('SignUpScreen')}/>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.inputs}>
+          <View>
+            <Text style={styles.inputText}>Username</Text>
+            <TextInput
+              style={styles.input}
+              onChange={text => setLogin(text)}
+            />
+          </View>
+          <View style={styles.inputPassword}>
+          <Text style={styles.inputText}>Password</Text>
+            <TextInput
+              style={styles.input}
+              onChange={text => setPassword(text)}
+              secureTextEntry
+            />
+          </View>
+        </View>
         <View style={styles.btn} >
-        <TouchableOpacity onPress={() => navigation.navigate('Tabs')}>
-            <Text>Sign in</Text>
-        </TouchableOpacity>
-
+          <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Tabs')}>
+            <Text style={{color: 'white', textTransform: "uppercase"}}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('SignUpScreen')}>
+              <Text style={styles.createBtn}>Create Account</Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     );
@@ -25,10 +44,49 @@ export default function SignInScreen({navigation}) {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: '#212237',
+    },
+    title: {
+      color: 'white',
+      fontSize: 24,
+      top: '20%',
+    },
+    inputs: {
+      width: '75%',
+      top: '35%',
+    },
+    input: {
+      borderBottomColor: '#999AA3',
+      borderBottomWidth: 1,
+      fontSize: 18,
+      color: '#B1B2B9',
+    },
+    inputText: {
+      fontSize: 10,
+      color: '#999AA3',
+      textTransform: "uppercase",
+      marginBottom: 12,
+    },
+    inputPassword: {
+      marginTop: 18,
     },
     btn: {
-        margin: 20
-    }
+      top: '50%',
+      alignItems: "center",
+    },
+    loginBtn: {
+      width: 285,
+      height: 40,
+      backgroundColor: '#F56C26',
+      color: 'white',
+      borderRadius: 5,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    createBtn: {
+      fontSize: 16,
+      color: '#999AA3',
+      marginTop: 25,
+    },
   });
   
--- src/views/SignUpScreen.js
+++ src/views/SignUpScreen.js
@@ -1,13 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import React, {useState} from "react";
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 
 export default function SignUpScreen({navigation}) {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeat, setRepeat] = useState('');
+
+  //validate func befor creating account
+
     return (
       <SafeAreaView style={styles.container}>
-        <Text>Sign up screen</Text>
-        <Button
-            title='Go to Sign In'
-            onPress={() => navigation.navigate('SignInScreen')}/>
+        <Text style={styles.title}>Create Account</Text>
+        <View style={styles.inputs}>
+          <View>
+            <Text style={styles.inputText}>Username</Text>
+            <TextInput
+              style={styles.input}
+              onChange={text => setLogin(text)}
+            />
+          </View>
+          <View style={styles.inputPassword}>
+          <Text style={styles.inputText}>Password</Text>
+            <TextInput
+              style={styles.input}
+              onChange={text => setPassword(text)}
+              secureTextEntry
+            />
+          </View>
+          <View style={styles.inputPassword}>
+          <Text style={styles.inputText}>Repeat Password</Text>
+            <TextInput
+              style={styles.input}
+              onChange={text => setRepeat(text)}
+              secureTextEntry
+            />
+          </View>
+        </View>
+        <View style={styles.btn} >
+          <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Tabs')}>
+            <Text style={{color: 'white', textTransform: "uppercase"}}>Create</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('SignInScreen')}>
+              <Text style={styles.createBtn}>Have an account? Login</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     );
   }
@@ -19,7 +55,49 @@ export default function SignUpScreen({navigation}) {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: '#212237',
+    },
+    title: {
+      color: 'white',
+      fontSize: 24,
+      top: '20%',
+    },
+    inputs: {
+      width: '75%',
+      top: '30%',
+    },
+    input: {
+      borderBottomColor: '#999AA3',
+      borderBottomWidth: 1,
+      fontSize: 18,
+      color: '#B1B2B9',
+    },
+    inputText: {
+      fontSize: 10,
+      color: '#999AA3',
+      textTransform: "uppercase",
+      marginBottom: 12,
+    },
+    inputPassword: {
+      marginTop: 18,
+    },
+    btn: {
+      top: '42%',
+      alignItems: "center",
+    },
+    loginBtn: {
+      width: 285,
+      height: 40,
+      backgroundColor: '#F56C26',
+      color: 'white',
+      borderRadius: 5,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    createBtn: {
+      fontSize: 16,
+      color: '#999AA3',
+      marginTop: 25,
     },
   });
   
